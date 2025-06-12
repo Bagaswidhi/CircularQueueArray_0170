5a4af672677520cd413072d737cab9fbf227b6d8
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-12 10:26:47 +0700

    Menambahkan contructor class Queues serta mengganti nama variable array

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -4,7 +4,14 @@ using namespace std;
 class Queues{
     private: 
         static const int max = 5;
-        int arr[max];
+        int queue_array[max];
         int FRONT;
         int REAR;
+
+    public:
+        Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
 };
