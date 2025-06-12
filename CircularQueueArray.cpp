fe86f12b21dbad33ce1b8e58503d456617d60428
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-12 10:33:37 +0700

    Menambahkan step 2 dimana melihat kosongnya antrian

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -25,5 +25,11 @@ class Queues{
                 cout << "\nQueue Overflow\n"; //1.a
                 return;
             } 
+
+            //2. Cek apakah antrian kosong
+            if (FRONT == -1){
+                FRONT = 0; // 2.a
+                REAR = 0; // 2.b
+            }
     }
 };
