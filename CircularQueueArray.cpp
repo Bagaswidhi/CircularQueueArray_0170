005a967fa1fcf5880bd6d012c505fa23a47ba57e
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-06-12 10:48:31 +0700

    Menambahkan decision bila FRONT lebih kecil atau sama dengan max -1 akan dilakukan perulangan

--- CircularQueueArray.cpp
+++ CircularQueueArray.cpp
@@ -83,5 +83,21 @@ class Queues{
                 }
                 cout << endl;
             }
+            else {
+                // Jika FRONT_position > REAR_position, Iterasi dari FRONT hingga akhir array
+                while (FRONT_position <= max -1){
+                    cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+
+                FRONT_position = 0; // Kembali ke awal array
+
+                // Iterasi dari awal array hingga REAR
+                while (FRONT_position <= REAR_position){
+                    cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+                cout << endl;
+            }
         }
 };
